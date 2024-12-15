8f4e843201118c5e3a5f4c33aae275ba22d0d1cc
frankassafuah <frankassafuah@gmail.com>
2024-12-15 14:56:18 +0000

    using ListCreateAPIView

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from todos.views import CreateTodoAPIView, TodoListApiView
+from todos.views import CreateTodoAPIView, TodoListApiView, ListCreateTodosAPIView
 
 
 urlpatterns = [
-    path("list/", TodoListApiView.as_view(), name="todos"),
+    path("", ListCreateTodosAPIView.as_view(), name="todos"),
+    path("list/", TodoListApiView.as_view(), name="list-todos"),
     path("create/", CreateTodoAPIView.as_view(), name="create-todo"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from todos.serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from todos.models import Todo
@@ -23,5 +23,18 @@ class TodoListApiView(ListAPIView):
     serializer_class = TodoSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user).all()
+
+
+
+# Shortcut to List and Create Todos. Does the samething as the above views
+class ListCreateTodosAPIView(ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+    
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user).all()
